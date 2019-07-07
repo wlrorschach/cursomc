6dfb6a28eca3a4c1ee1586b69a04a953d48a184d
wlrorachach <wlrorschach@gmail.com>
2019-07-07 16:55:03 -0300

    REST testado

--- src/main/java/com/wiliam/cursomc/CursomcApplication.java
+++ src/main/java/com/wiliam/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.wiliam.cursomc.cursomc;
+package com.wiliam.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
