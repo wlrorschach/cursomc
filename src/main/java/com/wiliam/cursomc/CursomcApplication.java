f3f1f3f53a5ff68dc8dc11167fef7993e46cbef5
wlrorachach <wlrorschach@gmail.com>
2019-07-09 13:20:57 -0300

    Acertando o H2

--- src/main/java/com/wiliam/cursomc/CursomcApplication.java
+++ src/main/java/com/wiliam/cursomc/CursomcApplication.java
@@ -2,8 +2,10 @@ package com.wiliam.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = { "cm.wiliam.cursomc" })
 public class CursomcApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/wiliam/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.wiliam.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wiliam.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
