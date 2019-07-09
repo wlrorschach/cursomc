f91dad2785317e1d50a5302d36d557baafd0a667
wlrorachach <wlrorschach@gmail.com>
2019-07-09 17:24:03 -0300

    Criando operacao de instanciacao

--- src/main/java/com/wiliam/cursomc/CursomcApplication.java
+++ src/main/java/com/wiliam/cursomc/CursomcApplication.java
@@ -1,15 +1,32 @@
 package com.wiliam.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
+import com.wiliam.cursomc.domain.Categoria;
+import com.wiliam.cursomc.repositories.CategoriaRepository;
 
 @SpringBootApplication
-@ComponentScan(value = { "/cursomc/src/main/java/com/wiliam/cursomc" })
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
--- src/main/java/com/wiliam/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/wiliam/cursomc/resource/CategoriaResource.java
@@ -17,11 +17,10 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaService service;
 	
-	@RequestMapping(value= "/(id)", method=RequestMethod.GET)
+	@RequestMapping(value= "/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
-		
 	}
 	
 }
