862fc8eb189f025987dfcc26d41415f136116ebb
wlrorachach <wlrorschach@gmail.com>
2019-07-11 17:50:13 -0300

    Tentaiva de corrigir erro no pom

--- src/main/java/com/wiliam/cursomc/domain/Produto.java
+++ src/main/java/com/wiliam/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.wiliam.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -31,6 +33,8 @@ public class Produto implements Serializable {
 	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
+	private Set<ItemPedido> itens = new HashSet<>();
+	
 	public Produto() {
 
 	}
@@ -42,6 +46,14 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
+	public List<Pedido> getPedidos(){
+		List<Pedido> lista = new ArrayList<>();
+		for (ItemPedido x : itens) {
+			lista.add(x.getPedido());
+		}
+		return lista;
+	}
+	
 	public Integer getId() {
 		return id;
 	}
@@ -74,6 +86,14 @@ public class Produto implements Serializable {
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
