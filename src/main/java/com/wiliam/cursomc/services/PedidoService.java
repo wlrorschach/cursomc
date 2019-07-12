771c7032cf9bd26811d19909769ec48c61e5a79b
wlrorachach <wlrorschach@gmail.com>
2019-07-11 22:02:35 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/wiliam/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.wiliam.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.wiliam.cursomc.domain.Pedido;
+import com.wiliam.cursomc.repositories.PedidoRepository;
+import com.wiliam.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! id:" 
+		+ Pedido.class.getName()
+		));
+	}
+}
