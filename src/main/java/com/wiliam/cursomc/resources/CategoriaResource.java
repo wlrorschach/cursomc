120ef4b04c9492a64f79cfec0ce304bb1ccbd22d
wlrorachach <wlrorschach@gmail.com>
2019-07-12 21:16:31 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/wiliam/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/wiliam/cursomc/resources/CategoriaResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -70,6 +72,18 @@ public class CategoriaResource {
 		
 		List<CategoriaDTO> listaDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		//List<CategoriaDTO> listaDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()));
+		return ResponseEntity.ok().body(listaDto);
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0")Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "nome")String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "ASC")String direction) {
+		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+		
+		Page<CategoriaDTO> listaDto = list.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listaDto);
 	}
 }
--- src/main/java/com/wiliam/cursomc/services/CategoriaService.java
+++ src/main/java/com/wiliam/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.wiliam.cursomc.domain.Categoria;
@@ -15,27 +18,31 @@ import com.wiliam.cursomc.services.exceptions.ObjectNotFoundException;
 // anotacao responsavel por dizer ao spring que esta classe eh uma classe de servico
 @Service
 public class CategoriaService {
-	
-	// anotacao responsavel por atribuir ao spring a responsabilidade de injetar esta dependencia
+
+	// anotacao responsavel por atribuir ao spring a responsabilidade de injetar
+	// esta dependencia
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
-		// o metodo 'findById()' eh responsavel por buscar  um elemento por id e retornarnar um Optional
+		// o metodo 'findById()' eh responsavel por buscar um elemento por id e
+		// retornarnar um Optional
 		Optional<Categoria> obj = repo.findById(id);
-		
-		// o metodo .orElseThrow() retorna um objeto e,m caso este seja null, lanca a excecao definida
-		return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! id:" 
-		+ Categoria.class.getName()
-		));
+
+		// o metodo .orElseThrow() retorna um objeto e,m caso este seja null, lanca a
+		// excecao definida
+		return obj.orElseThrow(
+				() -> new ObjectNotFoundException("Objeto não encontrado! id:" + Categoria.class.getName()));
 	}
-	
+
 	// metodo reponsável por inserir uma nova Categoria
 	public Categoria insert(Categoria obj) {
-		/* http status code : quando eh uma requisicao para insercao, o codigo é 201
-		ao inserir uma nova categoria, devo fornecer a nova URI como resposta a requisicao
-		*/
-		
+		/*
+		 * http status code : quando eh uma requisicao para insercao, o codigo é 201 ao
+		 * inserir uma nova categoria, devo fornecer a nova URI como resposta a
+		 * requisicao
+		 */
+
 		obj.setId(null);
 		// o metodo 'save' insere um novo registro quando o id == null
 		return repo.save(obj);
@@ -49,7 +56,7 @@ public class CategoriaService {
 	}
 
 	public void delete(Integer id) {
-		// 
+		//
 		find(id);
 		try {
 			repo.deleteById(id);
@@ -60,5 +67,10 @@ public class CategoriaService {
 
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
